63038cce217c2ea81db1be8fa01cde8b59230149
unknown <wstegem@utexas.edu>
2018-11-16 23:40:59 -0600

    updated time logging to use getTime() function

--- script.js
+++ script.js
@@ -11,32 +11,46 @@ firebase.initializeApp(config)
 
 var database = firebase.database()
 var schedule = []
-
-function timeToMinutes(time) {
-    return +time[0] * 60 + +time[1];
-}
+var dt = new Date().getTime()/60000
+dt = Math.floor(dt)
+var now = new Date()
+var dy = new Date(now.getFullYear(),now.getMonth(),now.getDate(),0,0,0)
+dy = Math.floor(dy.getTime()/60000)
 
 database.ref().on('value', function(snapshot) {
     schedule = snapshot.val().FBschedule;
     $('#table').empty()
-    var dt = new Date();
-    var currentTime = [dt.getHours(), dt.getMinutes()]
-   for(i=0;i<schedule.length;i++){
-    var days = 0
-    var timeArrival = timeToMinutes(schedule[i][3])
-    timeArrival = timeArrival + +schedule[i][2]
-    while(timeArrival<timeToMinutes(currentTime)){
-        timeArrival = timeArrival + +schedule[i][2]
-    }
-    var dayArrival = timeArrival
-    while(+dayArrival>1440){
-        dayArrival = +dayArrival - 1440
-        days++
-    }
-    if(days==0)
-        $('#table').append("<tr><th scope='row'>"+schedule[i][0]+"</th><td>"+schedule[i][1]+"</td><td>"+schedule[i][2]+" min</td><td>"+Math.floor(timeArrival/60)+":"+("0" + timeArrival%60).slice(-2)+"</td><td>"+(timeArrival-timeToMinutes(currentTime))+" min</td></tr>")
-    else
-        $('#table').append("<tr><th scope='row'>"+schedule[i][0]+"</th><td>"+schedule[i][1]+"</td><td>"+schedule[i][2]+" min</td><td>"+days+" days from now, at "+Math.floor(dayArrival/60)+":"+("0" + dayArrival%60).slice(-2)+"</td><td>"+(timeArrival-timeToMinutes(currentTime))+" min</td></tr>")
+    dt = new Date().getTime()/60000
+    dt = Math.floor(dt)
+    console.log('dt = '+dt)
+    now = new Date()
+    dy = new Date(now.getFullYear(),now.getMonth(),now.getDate(),0,0,0)
+    dy = Math.floor(dy.getTime()/60000)
+    console.log('dy = '+dy)
+
+    for(i=0;i<schedule.length;i++){
+        var days = 0
+        var timeArrival = schedule[i][3] + schedule[i][2]
+        console.log('timeArrival = '+timeArrival)
+        while(dt > timeArrival){
+            timeArrival = timeArrival + schedule[i][2]
+        }
+
+        var dayArrival = (timeArrival - dy)/60
+        console.log('dayArrival: '+dayArrival)
+        while(dayArrival>24){
+            dayArrival = dayArrival - 24
+            days++
+            console.log(dayArrival)
+        }
+        dayArrival = Math.floor(dayArrival)
+
+        if(days==0){
+            $('#table').append("<tr><th scope='row'>"+schedule[i][0]+"</th><td>"+schedule[i][1]+"</td><td>"+schedule[i][2]+" min</td><td>"+Math.floor((timeArrival-dy)/60)+":"+("0" + (timeArrival-dy)%60).slice(-2)+"</td><td>"+(timeArrival-dt)+" min</td></tr>")
+        }else{
+            dd = dy + (days*1440)
+            $('#table').append("<tr><th scope='row'>"+schedule[i][0]+"</th><td>"+schedule[i][1]+"</td><td>"+schedule[i][2]+" min</td><td>"+days+" days from now, at "+dayArrival+":"+("0" + (timeArrival-dd)%60).slice(-2)+"</td><td>"+(timeArrival-dt)+" min</td></tr>")
+        }
     }
 })
 
@@ -45,15 +59,17 @@ $("#add-train").on("click", function(event){
     if($('#form-name').val().trim()!=''&&$('#form-destination').val().trim()!=''&&$('#form-time').val().trim()!=''&&$('#form-freq').val().trim()!=''&&$('#form-freq').val().trim()!='0'){
         console.log($('#form-freq').val().trim())
         var newTrain = []
-        newTrain.push($('#form-name').val().trim())
-        newTrain.push($('#form-destination').val().trim())
-        newTrain.push($('#form-freq').val().trim())
-        var newTime = $('#form-time').val().trim().split(':')
-        newTrain.push(newTime)
-
+            newTrain.push($('#form-name').val().trim())
+            newTrain.push($('#form-destination').val().trim())
+            newTrain.push(+$('#form-freq').val().trim())
+            now = new Date()
+            dy = new Date(now.getFullYear(),now.getMonth(),now.getDate(),0,0,0)
+            dy = Math.floor(dy.getTime()/60000)
+            newTrain.push(dy + +$('#form-time').val().trim().split(':')[0]*60 + +$('#form-time').val().trim().split(':')[1])
+        
         schedule.push(newTrain)
         database.ref().set({
-        FBschedule: schedule
+            FBschedule: schedule
         })
     }else{
         window.alert('Invalid train entry')
